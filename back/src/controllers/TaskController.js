ba42821bc73075f9db3b56ca1756d3fb203dbcdb
Jaime <72435300+jsr-desarrolloweb@users.noreply.github.com>
2021-10-28 12:13:41 +0200

    wip: Añadidos endpoints /create y /update

--- back/src/controllers/TaskController.js
+++ back/src/controllers/TaskController.js
@@ -5,6 +5,7 @@ const { Task } = model;
 
 export default {
 
+    // READ
     async getAllTasks(req, res) {
         try {
             const tasks = await Task.findAll()
@@ -16,10 +17,57 @@ export default {
         } catch (error) {
             return res.status(500).send({
                 message: 'Could not perform operation at this time, kindly try again later.'
-            });
+            })
           }
+    },
+    
+    // CREATE
+    async createTask(req, res) {
+        
+        try {
+            const {name, isChecked} = req.body       
+            await Task.create({
+                name,
+                isChecked
+            })
+            return res.status(201).send({message: 'Task created successfully'})
+        } catch (error) {
+            return res.status(500).send({
+                message: 'Could not perform operation at this time, kindly try again later...'
+            })
         }
-    }
+    },
+
+    // UPDATE
+    async updateTask(req, res) {
+        try {
+            const task = await Task.findByPk(req.params.taskID)
+            if (task) {
+                const updatedTask = await Task.update({
+                    name: req.body.name
+                },
+                { where: { id:  req.params.taskID} }
+                )
+                res.status(201).send(updatedTask)
+            } else {
+                res.status(404).send("Task Not Found")
+            }
+            
+        } catch (error) {
+            console.log(error)
+            return res.status(500).send({
+                message: 'Could not perform operation at this time, kindly try again later...'
+            })      
+        }
+    },
+
+    // DELETE
+    
+}
+
+
+    
+
 
 //   async signUp(req, res) {
 //     const {email, password, name, phone} = req.body;
--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,5 +1,7 @@
 import TaskController from '../controllers/TaskController'
 
 export default (app) => {
-    app.get('/tasks', TaskController.getAllTasks);
-};
+    app.get('/tasks', TaskController.getAllTasks)
+    app.post('/task/create', TaskController.createTask)
+    app.put('/task/:taskID/update', TaskController.updateTask)
+}
